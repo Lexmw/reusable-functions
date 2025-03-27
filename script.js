90451e3fdb0d82e3ace2863056e26f10eb61ecfc
Lexius <lexius.waltar@gmail.com>
2025-03-26 21:42:52 -0400

    Creates the base code

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Function to calculate price after tax
 
+
+
+// Function to calculate total cost of two items
+
+
+// Function to get user input and calculate total cost
+
+
+// Call the function to start the program
